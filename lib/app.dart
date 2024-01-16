19fe57324a55d9cd9ccbd2183afed6cb6af7f2dc
Shalaby-VBS <shalaby.vbs@gmail.com>
2024-01-16 11:00:38 +0200

    Shalaby-VBS

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:expenses_tracker/screens/home/views/home_screen.dart';
+import 'package:expenses_tracker/screens/home/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
